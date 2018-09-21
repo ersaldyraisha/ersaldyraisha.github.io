3adafe95385c0420b9efa77676d07796861b5e68
Andi Ersaldy Raisha <andi.ersaldy@gmail.com>
2018-09-21 16:20:16 +0700

    content: more project added

--- dev/js/list.js
+++ dev/js/list.js
@@ -5,7 +5,13 @@ let projectList = [
     type: 'Mobile Application / 2018',
     role: 'UI Design + UX Researcher',
     desc: 'Institut Teknologi Sepuluh Nopember Surabaya is one of the best if not the best public tech university in Indonesia. As a highly claimed tech university, the ease of information access is a must to all of its stakeholder. myITS is a mobile application based on Android and iOS platform to accomodate all of the information needed for student, lecturer, staff, and parent/guardian ranging from academic information for student and lecturer to employee affair for staff.',
-    img: ['https://s3-ap-southeast-1.amazonaws.com/traveloka/imageResource/2017/03/17/1489744198907-356bed3012407aaa8f07132704bd7e66.png', 'https://cdn6.aptoide.com/imgs/5/9/d/59dc487eae29b1d8c929c057677a0765_screen.png?h=464', 'https://is5-ssl.mzstatic.com/image/thumb/Purple118/v4/59/25/3a/59253a9e-b196-e2cc-ca6b-fc2fc581c764/mzl.mcglrwxm.png/750x750bb.jpeg']
+    img : [
+      'assets/img/projects/myits.jpg',
+      'assets/img/projects/myits1.jpg',
+      'assets/img/projects/myits2.jpg',
+      'assets/img/projects/myits3.jpg',
+      'assets/img/projects/myits4.jpg'
+      ]
   },
   {
     id: 'masak',
@@ -34,7 +40,7 @@ let projectList = [
     type: 'Mobile Application / 2016',
     role: 'UI Design + Front-end Development',
     desc: 'Amman is a crowdsourced personal security application prototype based on Android platform that ensure your safety anywhere you go. Amman will notify you as you go somewhere with a high number of crime activities and will let you get emergency service if the unwanted thing happen. This project has won several awards such as a silver medal at Multimedia & Game Event ITS 2016 and finalist at Go-Jek GO-HACKATHON.',
-    img: ['http://www.bukalapak.com/images/logo-google-graph.png', 'https://cdn6.aptoide.com/imgs/5/9/d/59dc487eae29b1d8c929c057677a0765_screen.png?h=464', 'https://is5-ssl.mzstatic.com/image/thumb/Purple118/v4/59/25/3a/59253a9e-b196-e2cc-ca6b-fc2fc581c764/mzl.mcglrwxm.png/750x750bb.jpeg']
+    img: ['assets/img/projects/amann.jpg', 'assets/img/projects/amann1.jpg', 'assets/img/projects/amann2.jpg', 'assets/img/projects/amann3.jpg']
   },
   {
     id: 'lp2',
@@ -42,7 +48,7 @@ let projectList = [
     type: 'Website  / 2015',
     role: 'Front-end Development',
     desc: 'Programming Laboratory II (LP2) is one of the service laboratory in Informatics Department ITS that can be reserved for any classes or events. With the increasing usage of the reservation, a platform is built to simplify the reservation process with minimum of fuss.',
-    img: ['http://www.bukalapak.com/images/logo-google-graph.png', 'https://cdn6.aptoide.com/imgs/5/9/d/59dc487eae29b1d8c929c057677a0765_screen.png?h=464', 'https://is5-ssl.mzstatic.com/image/thumb/Purple118/v4/59/25/3a/59253a9e-b196-e2cc-ca6b-fc2fc581c764/mzl.mcglrwxm.png/750x750bb.jpeg']
+    img: ['assets/img/projects/lp2.jpg', 'https://cdn6.aptoide.com/imgs/5/9/d/59dc487eae29b1d8c929c057677a0765_screen.png?h=464', 'https://is5-ssl.mzstatic.com/image/thumb/Purple118/v4/59/25/3a/59253a9e-b196-e2cc-ca6b-fc2fc581c764/mzl.mcglrwxm.png/750x750bb.jpeg']
   }
 ]
 
